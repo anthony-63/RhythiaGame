21c22299636c18284c3421e43d3806642ec6f60f
anthony-63 <back.up.email.158@gmail.com>
2024-06-24 11:57:57 -0400

    new game impl

--- Rhythia.Game/Scenes/Game/Game.cs
+++ Rhythia.Game/Scenes/Game/Game.cs
@@ -1,17 +1,24 @@
+using System.Numerics;
 using Rhythia.Engine;
-using Rhythia.Engine.GFX._3D;
+using Rhythia.Engine.GFX;
 using Rhythia.Engine.Scene;
 
 namespace Rhythia.Game.Scenes.Game;
 
 public class GameScene : IScene
 {
+    Camera Camera = new Camera(new Vector3(0, 0, -7), 70);
+    Sprite Grid = Sprite.MakePlane(new Vector3(0, 0, 0), new Vector3(90, 0, 0), new Vector2(6, 6), "Assets/Game/Grid.png");
 
     public void Update(Window window, double dt)
     {
+
     }
 
     public void Render(Window window)
     {
+        Camera.Start();
+        Grid.Render();
+        Camera.End();
     }
 }
