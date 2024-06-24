958807e87c330159eb2cd1ad839b56d55cc29994
anthony-63 <back.up.email.158@gmail.com>
2024-06-24 11:57:44 -0400

    window impl for raylib

--- Rhythia.Engine/Window.cs
+++ Rhythia.Engine/Window.cs
@@ -1,5 +1,7 @@
 using Rhythia.Engine.Scene;
 
+using Raylib_cs;
+
 namespace Rhythia.Engine;
 
 public class Window
@@ -8,6 +10,23 @@ public class Window
 
     public Window(int width, int height, string title)
     {
+        Raylib.InitWindow(width, height, title);
+    }
 
+    public void Run()
+    {
+        while(!Raylib.WindowShouldClose())
+        {
+            Raylib.PollInputEvents();
+            SceneHandler.UpdateAllScenes(this, Raylib.GetFrameTime());
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+
+            SceneHandler.RenderAllScenes(this);
+
+            Raylib.EndDrawing();
+
+        }
     }
 }
