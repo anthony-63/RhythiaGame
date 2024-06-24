dd9cae113aad634e71195c37e45c8cdbe5d04195
anthony-63 <back.up.email.158@gmail.com>
2024-06-23 21:06:23 -0400

    cleanup SceneHandler

--- Rhythia.Engine/Scene/SceneHandler.cs
+++ Rhythia.Engine/Scene/SceneHandler.cs
@@ -6,20 +6,19 @@ public class SceneHandler
 
     List<IScene> RemovalQueue = [];
 
-    public void UpdateAllScenes(Window window, double dt) {
-        foreach(var scene in Scenes) {
+    public void UpdateAllScenes(Window window, double dt)
+    {
+        foreach(var scene in Scenes)
             scene.Update(window, dt);
-        }
 
-        foreach(var scene in RemovalQueue) {
+        foreach(var scene in RemovalQueue)
             Scenes.Remove(scene);
-        }
     }
 
-    public void RenderAllScenes(Window window) {
-        foreach(var scene in Scenes) {
+    public void RenderAllScenes(Window window)
+    {
+        foreach(var scene in Scenes)
             scene.Render(window);
-        }
     }
 
     public void ClearScenes() {
@@ -30,9 +29,12 @@ public class SceneHandler
         Scenes.Add(scene);
     }
 
-    public void RemoveSceneByType(Type type) {
-        for(int i = 0; i < Scenes.Count; i++) {
-            if(Scenes[i].GetType() == type) {
+    public void RemoveSceneByType(Type type)
+    {
+        for(int i = 0; i < Scenes.Count; i++)
+        {
+            if(Scenes[i].GetType() == type)
+            {
                 RemovalQueue.Add(Scenes[i]);
                 break;
             }
