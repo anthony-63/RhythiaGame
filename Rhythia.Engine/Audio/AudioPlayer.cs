3d62c71ef2b802b4e2af29d694f6ff304ba63f73
anthony-63 <back.up.email.158@gmail.com>
2024-06-25 11:01:28 -0400

    Synced and smooth audio player

--- Rhythia.Engine/Audio/AudioPlayer.cs
+++ Rhythia.Engine/Audio/AudioPlayer.cs
@@ -24,15 +24,10 @@ public class AudioPlayer
     public AudioPlayer(byte[] data, float volume, float speed = 1f)
     {
         AudioData = data;
-        AudioStream = Raylib.LoadMusicStreamFromMemory(GetFileFormat(AudioData), AudioData);
+        AudioStream = Raylib.LoadMusicStreamFromMemory(AudioUtil.GetFileFormat(AudioData), AudioData);
         Raylib.SetMusicVolume(AudioStream, volume);
         Raylib.SetMusicPitch(AudioStream, speed);
         Speed = speed;
-    }
-
-    public void Play()
-    {
-        Raylib.PlayMusicStream(AudioStream);
     }
 
     public void Play(float from)
@@ -50,13 +45,4 @@ public class AudioPlayer
     {
         Raylib.UpdateMusicStream(AudioStream);
     }
-
-    private static string GetFileFormat(byte[] bytes)
-	{
-		if (bytes.Length < 10) return "unknown";
-		if (bytes[0] == 0x52 && bytes[1] == 0x49 && bytes[2] == 0x46 && bytes[3] == 0x46) return ".wav";
-		if ((bytes[0] == 0xFF && (bytes[1] == 0xFB || (bytes[1] == 0xFA && bytes[2] == 0x90))) || (bytes[0] == 0x49 && bytes[1] == 0x44 && bytes[2] == 0x33)) return ".mp3";
-		if (bytes[0] == 0x4F && bytes[1] == 0x67 && bytes[2] == 0x67 && bytes[3] == 0x53) return ".ogg";
-		return "unknown";
-	}
 }
