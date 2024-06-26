269337d68f496aff7e42151be2aeff92a0369cc0
anthony-63 <back.up.email.158@gmail.com>
2024-06-26 10:47:09 -0400

    CursorSettings

---
+++ Rhythia.Content/Settings/CursorSettings.cs
@@ -0,0 +1,8 @@
+namespace Rhythia.Content.Settings;
+
+public class CursorSettings
+{
+    public bool Clamped = true;
+    public float Sensitivity = 0.5f;
+    public float Scale = 0.5f;
+}
--- Rhythia.Content/Settings/Settings.cs
+++ Rhythia.Content/Settings/Settings.cs
@@ -3,4 +3,5 @@ namespace Rhythia.Content.Settings;
 public class Settings
 {
     public NoteSettings Note = new NoteSettings();
+    public CursorSettings Cursor = new CursorSettings();
 }
