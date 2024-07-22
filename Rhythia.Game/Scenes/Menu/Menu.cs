15a65d25928dcd77b4c21b16420febbd659eb5ad
anthony-63 <back.up.email.158@gmail.com>
2024-07-22 18:42:04 -0400

    janky button impl

--- Rhythia.Game/Scenes/Menu/Menu.cs
+++ Rhythia.Game/Scenes/Menu/Menu.cs
@@ -17,6 +17,27 @@ public class MenuScene : IScene {
         AlignmentX = TextAlignX.Left,
         AlignmentY = TextAlignY.Top,
     };
+    public Button TestButton = new() {
+        Size = new UDim2(0.2f, 0, 0.2f, 0),
+        Position = new UDim2(0.5f, 0, 0.5f, 0),
+        Anchor = UiElementAnchor.Center,
+        NormalFrame = new Frame {
+            Color = Color.Gray,
+        },
+        HoveringFrame = new Frame {
+            Color = Color.DarkGray,
+        },
+        PressedFrame = new Frame {
+            Color = Color.DarkPurple,
+        },
+        Label = new Label {
+            Size = new UDim2(1f, 0, 1f, 0),
+            Position = UDim2.Zero,
+            AlignmentX = TextAlignX.Center,
+            AlignmentY = TextAlignY.Middle,
+        }
+    };
+    
 
     public MenuScene() {
         TestUI.Children.Add(new UiElement {
@@ -27,36 +48,20 @@ public class MenuScene : IScene {
                     Position = new UDim2(0f, 0, 0f, 0),
                     ImagePath = "Assets/Game/cat.png",
                 },
-                new Frame {
-                    Size = new UDim2(0.2f, 0, 0.2f, 0),
-                    Position = new UDim2(0.5f, 0, 0.5f, 0),
-                    Anchor = UiElementAnchor.Center,
-                    Color = Color.White,
-                    Children = {
-                        new Frame {
-                            Size = new UDim2(0.8f, 0, 0.8f, 0),
-                            Position = new UDim2(0.5f, 0, 0.5f, 0),
-                            Color = Color.Brown,
-                            Anchor = UiElementAnchor.Center,
-                            BorderWidth = 4,
-                            Roundness = 0.2f,
-                        },
-                    }
-                },
-                new Label {
-                    Size = new UDim2(1, 0, 1, 0),
-                    Text = "Test Label 0\nNew Line",
-                    FontSize = 18,
-                    AlignmentX = TextAlignX.Left,
-                    AlignmentY = TextAlignY.Bottom
-                },
-                new Label {
-                    Size = new UDim2(1, 0, 1, 0),
-                    Text = "Test Label 1\nNew Line",
-                    FontSize = 24,
-                    AlignmentX = TextAlignX.Center,
-                    AlignmentY = TextAlignY.Middle
-                },
+                // new Label {
+                //     Size = new UDim2(1, 0, 1, 0),
+                //     Text = "Test Label 0\nNew Line",
+                //     FontSize = 18,
+                //     AlignmentX = TextAlignX.Left,
+                //     AlignmentY = TextAlignY.Bottom
+                // },
+                // new Label {
+                //     Size = new UDim2(1, 0, 1, 0),
+                //     Text = "Test Label 1\nNew Line",
+                //     FontSize = 24,
+                //     AlignmentX = TextAlignX.Center,
+                //     AlignmentY = TextAlignY.Middle
+                // },
                 new Label {
                     Size = new UDim2(1, 0, 1, 0),
                     Text = "Test Label 2\nNew Line",
@@ -64,26 +69,27 @@ public class MenuScene : IScene {
                     AlignmentX = TextAlignX.Right,
                     AlignmentY = TextAlignY.Top
                 },
-                new Label {
-                    Size = new UDim2(0, 96, 1, 0),
-                    Text = "Wrapping Label testtesttesttesttesttesttesttest",
-                    TextWrapped = true,
-                    FontSize = 24,
-                    AlignmentX = TextAlignX.Left,
-                    AlignmentY = TextAlignY.Middle
-                },
-                new Label {
-                    Size = new UDim2(0, 96, 1, 0),
-                    Position = new UDim2(1, -96, 0, 0),
-                    Text = "Wrapping Label 2 testtesttesttesttesttesttesttest",
-                    TextWrapped = true,
-                    FontSize = 24,
-                    AlignmentX = TextAlignX.Right,
-                    AlignmentY = TextAlignY.Middle
-                }
+                // new Label {
+                //     Size = new UDim2(0, 96, 1, 0),
+                //     Text = "Wrapping Label testtesttesttesttesttesttesttest",
+                //     TextWrapped = true,
+                //     FontSize = 24,
+                //     AlignmentX = TextAlignX.Left,
+                //     AlignmentY = TextAlignY.Middle
+                // },
+                // new Label {
+                //     Size = new UDim2(0, 96, 1, 0),
+                //     Position = new UDim2(1, -96, 0, 0),
+                //     Text = "Wrapping Label 2 testtesttesttesttesttesttesttest",
+                //     TextWrapped = true,
+                //     FontSize = 24,
+                //     AlignmentX = TextAlignX.Right,
+                //     AlignmentY = TextAlignY.Middle
+                // }
             }
         });
         TestUI.Children.Add(FPSLabel);
+        TestUI.Children.Add(TestButton);
     }
 
     public void Render(Window window) {
@@ -92,6 +98,11 @@ public class MenuScene : IScene {
 
     public void Update(Window window, double dt) {
         FPSLabel.Text = $"FPS: {Raylib.GetFPS()}";
+        switch(TestButton.State) {
+            case ButtonState.Normal: TestButton.Label.Text = "Not Hovering"; break;
+            case ButtonState.Hovering: TestButton.Label.Text = "Hovering"; break;
+            case ButtonState.Pressed: TestButton.Label.Text = "Pressed"; break;
+        }
         TestUI.Update(dt);
     }
 }
