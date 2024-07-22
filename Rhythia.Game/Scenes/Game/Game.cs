7d701b73bd7568f29f8adf4fb980981303633b93
anthony-63 <back.up.email.158@gmail.com>
2024-07-22 19:22:58 -0400

    go to menu if map isnt selected

--- Rhythia.Game/Scenes/Game/Game.cs
+++ Rhythia.Game/Scenes/Game/Game.cs
@@ -6,6 +6,7 @@ using Rhythia.Engine.GFX;
 using Rhythia.Engine.Scene;
 using Rhythia.Game.Scenes.Game.NoteObject;
 using Rhythia.Game.Scenes.Game.Player;
+using Rhythia.Game.Scenes.Menu;
 
 namespace Rhythia.Game.Scenes.Game;
 
@@ -13,16 +14,23 @@ public class GameScene : IScene {
     public Player.Player Player = new();
     public Sprite Grid = Sprite.MakePlane(new Vector3(0, 0, 0), new Vector3(90, 0, 180), new Vector2(6, 6), "Assets/Game/Grid.png");
 
-    public SyncAudioPlayer Music = new(Global.DemoMap?.AudioData ?? [], 0.1f);
+    public SyncAudioPlayer Music = new(Global.SelectedMap?.AudioData ?? null, 0.1f);
 
     public NoteObjectSpawner? Spawner = null;
     public NoteObjectRenderer? Renderer = null;
 
-    public GameScene() {
+    public GameScene() {        
         Raylib.DisableCursor();
     }
 
     public void Update(Window window, double dt) {
+        if(Global.SelectedMap == null) {
+            window.SceneHandler.RemoveSceneByType<GameScene>();
+            window.SceneHandler.AddScene(new MenuScene());
+            Raylib.EnableCursor();
+            return;
+        }
+
         Spawner ??= new NoteObjectSpawner(this, Player);
         Renderer ??= new NoteObjectRenderer(this);
 
